b4da0f97c0cccb89a4fc84db3d64b3e2e79aa079
Aris Setiana <72619137+arezbidam@users.noreply.github.com>
2024-12-30 13:50:42 +0700

    fitur: backend - blok tersedia

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlokTersediaController;
 use App\Http\Controllers\FasilitasController;
 use App\Http\Controllers\FasilitasSekitarController;
 use App\Http\Controllers\ProjectController;
@@ -12,3 +13,4 @@ Route::get('/', function () {
 Route::get('project/{id_project}', [ProjectController::class, 'index'])->name('project');
 Route::get('fasilitas/{id_project}', [FasilitasController::class, 'index'])->name('fasilitas');
 Route::get('fasilitas_sekitar/{id_project}', [FasilitasSekitarController::class, 'index'])->name('fasilitas.sekitar');
+Route::get('blok_tersedia/{id_project}', [BlokTersediaController::class, 'index'])->name('blok.tersedia');
