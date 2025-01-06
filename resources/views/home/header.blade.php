007a92a93c31da3b2de3d8d58af88733cf33ca1a
khaerul umam <46287068+khael6690@users.noreply.github.com>
2025-01-06 17:29:35 +0700

    fix: landing page banner

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -4,58 +4,14 @@
 <header class="header-area" id="sticky-header">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-3">
+            <div class="col-lg-10">
                 <div class="logo">
-                    <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+                    <a href="{{ url('/') }}"><img src="{{ url('/assets/images/fav-icon') }}/icon-ningrat.png"
+                            alt="" style="height: 50px"></a>
                 </div>
             </div>
-            <div class="col-lg-9">
-                <div class="header-menu">
-                    <ul>
-                        <li><a href="#">Home <i class="bi bi-chevron-down"></i></a>
-                            <div class="sub-menu">
-                                <ul>
-                                    <li><a href="index.html">Home </a></li>
-                                    <li><a href="index-two.html">Home Two</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="#">Services <i class="bi bi-chevron-down"></i></a>
-                            <div class="sub-menu">
-                                <ul>
-                                    <li><a href="service.html">Service</a></li>
-                                    <li><a href="service-details.html">Service Details</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><a href="#">Pages <i class="bi bi-chevron-down"></i></a>
-                            <div class="sub-menu">
-                                <ul>
-                                    <li><a href="project-grid.html">Project</a></li>
-                                    <li><a href="project-details.html">Project Details</a></li>
-                                    <li><a href="team.html">Team</a></li>
-                                    <li><a href="team-detials.html">Team Details</a></li>
-                                    <li><a href="faq.html">Faq Page</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                    <li><a href="error.html">Error Page</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><a href="#">Blog <i class="bi bi-chevron-down"></i></a>
-                            <div class="sub-menu">
-                                <ul>
-                                    <li><a href="blog.html">Blog</a></li>
-                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><a href="contact.html">Contacts</a></li>
-                    </ul>
-                    <div class="hamilton-btn">
-                        <a href="contact.html">Get a Quout</a>
-                    </div>
-                </div>
+            <div class="col-lg-2">
+                <button type="button" class="btn btn-primary btn-sm">Masuk</button>
             </div>
         </div>
     </div>
@@ -65,7 +21,8 @@
 <div class="mobile-menu-area sticky-menu" id="navbar">
     <div class="mobile-menu">
         <div class="mobile-logo">
-            <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+            <a href="{{ url('/') }}"><img src="{{ url('/assets/images/fav-icon') }}/icon-ningrat.png"
+                    alt=""></a>
         </div>
         <div class="side-menu-info">
             <div class="sidebar-menu">
