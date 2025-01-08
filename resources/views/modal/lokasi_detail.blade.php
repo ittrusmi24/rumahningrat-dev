6a7dda9297bc14092b84ed32b06950b6dc57d2e2
Aris Setiana <72619137+arezbidam@users.noreply.github.com>
2025-01-08 11:19:30 +0700

    detail selengkapnya

--- resources/views/modal/lokasi_detail.blade.php
+++ resources/views/modal/lokasi_detail.blade.php
@@ -20,45 +20,16 @@
                                     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                                 </iframe>
                             </div>
-                            <div class="p-2">
+                            <div class="ps-4 pe-4 pt-2">
                                 <h5>{{ $project['title_satu'] }}</h5>
                                 <h5>{{ $project['title_dua'] }}</h5>
                                 <p>{{ $project['alamat'] }}</p>
-                                <button type="button" class="btn btn-secondary float-right"
+                                <button type="button" class="btn btn-secondary float-end"
                                     data-bs-dismiss="modal">Close</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="modal_video" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content rounded-4">
-            <div class="modal-body modal-body p-0 m-0">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h5 class="modal-title">Video Kawasan</h5>
-                    </div>
-                    <div class="col-md-4" align="right">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-body bg-primary" style="padding: 0px;">
-                {{-- <div class="video">
-                    <iframe id="video_presentation" width="100%" height="281" src="" title="Rumah Ningrat"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                    </iframe>
-                </div> --}}
             </div>
         </div>
     </div>
