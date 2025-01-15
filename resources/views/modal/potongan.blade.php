78c7d00d8a4ba9e9ffe1c1fa7749d20aab1ec19f
khaerul umam <46287068+khael6690@users.noreply.github.com>
2025-01-15 16:13:38 +0700

    feat: agreement potongan harga

---
+++ resources/views/modal/potongan.blade.php
@@ -0,0 +1,24 @@
+ <div class="modal fade" id="agreementModal" tabindex="-1" aria-labelledby="agreementModalLabel" aria-hidden="true">
+     <div class="modal-dialog">
+         <div class="modal-content">
+             <div class="modal-header">
+                 <h5 class="modal-title" id="agreementModalLabel">Agreement</h5>
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+             </div>
+             <div class="modal-body">
+                 <p>
+                     Please read and agree to the terms and conditions before proceeding.
+                 </p>
+                 <div class="form-check">
+                     <input class="form-check-input" type="checkbox" id="agreeCheckbox">
+                     <label class="form-check-label" for="agreeCheckbox">
+                         I agree to the terms and conditions.
+                     </label>
+                 </div>
+             </div>
+             <div class="modal-footer">
+                 <button type="button" class="btn btn-primary btn-sm" id="okButton" disabled>OK</button>
+             </div>
+         </div>
+     </div>
+ </div>
