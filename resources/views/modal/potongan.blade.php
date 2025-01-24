3502b870c945839144d6e6ce7585da4312b426a9
sidiq-eq <indasidik@gmail.com>
2025-01-24 17:01:59 +0700

    fix aggrement text

--- resources/views/modal/potongan.blade.php
+++ resources/views/modal/potongan.blade.php
@@ -2,17 +2,21 @@
      <div class="modal-dialog">
          <div class="modal-content">
              <div class="modal-header">
-                 <h5 class="modal-title" id="agreementModalLabel">Agreement</h5>
+                 <h5 class="modal-title" id="agreementModalLabel">Syarat dan Ketentuan Potongan
+                 </h5>
                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
              </div>
              <div class="modal-body">
-                 <p>
-                     Please read and agree to the terms and conditions before proceeding.
-                 </p>
+                 <ol class="small text-dark">
+                    <li>Apabila Kamu dinyatakan Lolos BI Checking dan lanjut ke tahap Pemberkasan, maka wajib membayar biaya sesuai dengan ketentuan berikut
+                    </li>
+                    <li>Setelah terbit SP3K dari Bank, maka Kamu wajib melunasi sisa dari Total Biaya yang belum dibayar selambat-lambatnya 7 hari sejak tanggal terbit SP3K</li>
+                    <li>Apabila tidak dapat membayar/melunasi sesuai dengan ketentuan di atas, maka system akan menyatakan Reject dan pemberian potongan akan dibatalkan secara otomatis serta akan terkena potongan sesuai ketentuan berikut</li>
+                 </ol>
                  <div class="form-check">
                      <input class="form-check-input" type="checkbox" id="agreeCheckbox">
                      <label class="form-check-label" for="agreeCheckbox">
-                         I agree to the terms and conditions.
+                         Saya setuju
                      </label>
                  </div>
              </div>
